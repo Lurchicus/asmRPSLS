a6d176f9d4f3b684cb840892aae91ca7df61aed5
Dan Rhea <dan.rhea@gmail.com>
2024-12-15 02:39:55 -0500

    Added debug prints to the random routine

--- rpsls.asm
+++ rpsls.asm
@@ -396,22 +396,44 @@ section	.text
 	push	rbp		; Save the program counter
 	mov	rbp, rsp	; Move the stack pointer to the program counter
 	; =======
-	mov rax, 2
-	mov [rando], rax
-	jmp .lret
+	mov 	rax, 1		; Force 1
+	mov 	[rando], rax	; Save it
+	mov 	rax, NOFLOAT	; non-float output
+	mov 	rdi, numnl	; number format (%d)
+	mov 	rsi, [rando]	; computer selection
+	call 	printf
 	; =======
 	xor	rax, rax	; zero rax
 	mov	rcx, rax	; move rax to rcx
 	call	time 		; Get the current time (seed for random)
+
+	; =======
+	mov 	rax, 2		; Force 2
+	mov 	[rando], rax	; Save it
+	mov 	rax, NOFLOAT	; non-float output
+	mov 	rdi, numnl	; number format (%d)
+	mov 	rsi, [rando]	; computer selection
+	call 	printf
+	; =======
+
 	mov	rcx, rax	; Save rax to rcx again
 	call	srand 		; Seed random number procedure
+	; =======
+	mov 	rax, 3		; Force 3
+	mov 	[rando], rax	; Save it
+	mov 	rax, NOFLOAT	; non-float output
+	mov 	rdi, numnl	; number format (%d)
+	mov 	rsi, [rando]	; computer selection
+	call 	printf
+	; =======
+
 	call	rand		; call the random number procedure
 
 	; rand = (rand % 4) + 1
 	xor	rdx, rdx	; Clear rdx
-	mov	rcx, 4		; 0 to 4 +1 (I think)
+	mov	rcx, 5		; 0 to 4 +1 (I think)
 	div 	rcx		; Get the value
-	;inc 	rdx 		; +1 for 1 through 5
+	;inc 	rdx 		; +1 for 1 through 5 (one base)
 	mov	[rando], rdx	; Save it 
 
 .lret:
